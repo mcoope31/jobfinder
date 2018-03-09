ffc3956fb5b35a67f43d8ba1a8223e99dadd7c4b
Matthew Nicolay <mnicolay95@gmail.com>
2018-03-08 20:48:09 -0600

    Fixed problem with education section of job page

--- resources/views/job/job.blade.php
+++ resources/views/job/job.blade.php
@@ -16,25 +16,31 @@
     <div class="row">
 
         <div class="col-md-3">
-        
+
             <?php $img_src = '/storage/company_images/company'.$job->company_id.'.png'; ?>
             @if(file_exists(public_path($img_src)))
                 <img style="width:250px;height:250px;" src="{{$img_src}}?={{ File::lastModified(public_path().'/'.$img_src) }}">
             @else
                 <img style='width:250px;height:250px;' src='/storage/company_images/noimage.png'>
             @endif
-            
+
             <a href="{{ url('/profile/'.$job->company_id) }}" class="btn btn-primary btn-block" style="margin-top:10px;">View Company</a>
         </div>
         <div class="col-md-9">
-            
+
             <p style="font-size:2em;">
                 {{ $job->title }}
                 <span class="float-right badge badge-primary" style="font-size:0.5em;">Created: {{ $job->created_at->diffForHumans() }}</span>
             </p>
             <p style="font-size:1.5em;">
                 <span class="badge badge-secondary">{{ $job->type }}</span>
-                <span class="badge badge-secondary">Education: {{ $job->education }}</span>
+                <span class="badge badge-secondary">Education:
+                    @if($job->education == 0)
+                        Not Necessary
+                    @else
+                        {{ $job->education_level->education }}
+                    @endif
+                </span>
                 <span class="badge badge-secondary">Experience: {{ $job->experience }}</span>
             </p>
             <p>
@@ -55,7 +61,7 @@
                 <strong>Company: </strong> {{ $job->company->name }}
             </p>
             <p>
-                <strong>Location: </strong> 
+                <strong>Location: </strong>
                 @if(!empty($job->company->city) && !empty($job->company->state))
                     {{ $job->company->city }}, {{ $job->company->state }} {{ $job->company->zipcode }}
                 @else
@@ -63,7 +69,7 @@
                 @endif
             </p>
             <p>
-                <strong>Contact Email: </strong> 
+                <strong>Contact Email: </strong>
                 @if(empty($job->company->contact_email))
                     {{ $job->company->user->email }}
                 @else
@@ -71,7 +77,7 @@
                 @endif
             </p>
             <p>
-                <strong>Phone: </strong> 
+                <strong>Phone: </strong>
                 @if(!empty($job->company->phone))
                     {{ $job->company->phone }}
                 @else
@@ -82,7 +88,7 @@
     </div>
     <hr>
     <div class="row">
-        
+
         <div class="col-md-6">
             <div class="form-group">
                 <a href="{{ URL::previous() }}" class="btn btn-danger btn-block">Back</a>
@@ -103,7 +109,7 @@
                 </div>
             @endif
         </div>
-        
+
     </div>
 
 @endsection
