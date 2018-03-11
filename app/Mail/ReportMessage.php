23bd1ef7839109bf2dbbb030ac385334637661c2
mcoope31 <mscoops@gmail.com>
2018-03-11 16:36:06 -0500

    Fixed email messages again 2...

--- app/Mail/ReportMessage.php
+++ app/Mail/ReportMessage.php
@@ -42,7 +42,7 @@ class ReportMessage extends Mailable
         $title = 'Job #'.$this->id.' ('.$this->name.') has been reported';
         if($this->type == 'user') $title = 'User #'.$this->id.' ('.$this->name.', '.$this->email.') has been reported';
         $content = $this->message;
-        return $this->to(env('MAIL_USERNAME',''))->subject($this->name.' has been reported')->from(env('MAIL_USERNAME',''))
+        return $this->to(env('MAIL_USERNAME','jobfinderCSC396@gmail.com'))->subject($this->name.' has been reported')->from(env('MAIL_USERNAME','jobfinderCSC396@gmail.com'))
             ->view('emails.send',compact('title', 'content'));
     }
 }
